830c26f010c9b04032a7cde83a186903c365fb88
Dakota <bazookaharwell@gmail.com>
2017-04-03 18:11:53 -1000

    sorted

---
+++ app.js
@@ -0,0 +1,17 @@
+module.exports = (arr) => {
+  let isSorted = false;
+  let storage = null;
+
+  while (!isSorted) {
+    isSorted = true;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i-1] > arr[i]) {
+        storage = arr[i];
+        arr[i] = arr[i-1];
+        arr[i-1] = storage;
+        isSorted = false;
+      }
+    }
+  }
+  return arr;
+};
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,11 +1,26 @@
 var chai = require('chai');
+var expect = chai.expect;
 var bubbleSort = require('../app.js');
 
 describe('Bubble Sort', () => {
 
   it('should sort an array', () => {
-    expect(bubbleSort([4, 3, 2, 1])).to.be.equal([1, 2, 3, 4]);
-    expect(bubbleSort([8, 21, 2, 4, 7, 3])).to.be.equal([2, 3, 4, 7, 8, 21]);
+    expect(bubbleSort([3, 4, 2, 1])).to.be.deep.equal([1, 2, 3, 4]);
+    expect(bubbleSort([8, 21, 2, 4, 7, 3])).to.be.deep.equal([2, 3, 4, 7, 8, 21]);
+
+    let longArr = [];
+
+    for (let i = 100; i >= 0; i--) {
+      longArr.push(i);
+    }
+
+    let longArrSorted = [];
+
+    for (let i = 0; i <= 100; i++) {
+      longArrSorted.push(i);
+    }
+
+    expect(bubbleSort(longArr)).to.be.deep.equal(longArrSorted);
   });
 
 });
